57fab79524f161aeb27afdfdabd186546ebdc3ec
BonZaii2134 <96675601+BonZaii2134@users.noreply.github.com>
2022-08-25 17:59:58 +0545

    New SMPT Server

--- glss/info.py
+++ glss/info.py
@@ -1,8 +1,8 @@
 from pickle import TRUE
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = TRUE
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.sendinblue.com'
 EMAIL_HOST_USER = 'disyou2134@gmail.com'
-EMAIL_HOST_PASSWORD = 'Meow1234'
+EMAIL_HOST_PASSWORD = 'vPzgb98HOFw5BGKr'
 EMAIL_PORT = 587
