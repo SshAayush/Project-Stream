1db3b1075eb1efaa9a183ceb7e398dfcc1774748
Aayush Timalsina <96675601+SshAayush@users.noreply.github.com>
2023-03-02 15:54:58 +0545

    Removed info.py

--- glss/info.py
+++ glss/info.py
@@ -1,8 +1,1 @@
-from pickle import TRUE
-
-
-EMAIL_USE_TLS = TRUE
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'disyou2134@gmail.com'
-EMAIL_HOST_PASSWORD = 'Meow1234'
-EMAIL_PORT = 587
+#add smtp server info
